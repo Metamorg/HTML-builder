d8e095bfc87361811088d33f00380b46fda3bdbd
Metamorg <egoronokasd@gmail.com>
2022-05-23 22:51:04 +0300

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
 
+const path = require('path');
+const { readdir, copyFile, rm, mkdir } = require('fs/promises');
+
+const main = path.join(__dirname, 'files');
+const copy  = path.join(__dirname, 'files-copy');
+
+async function copyDir(main, copy) {
+  try{
+    await rm(copy, { recursive: true, force: true  });
+    await mkdir(copy);
+  
+    await readdir(main, {withFileTypes: true},(err, items) =>{
+      if(err) {
+        console.log(err);
+      } else {
+        items.forEach(file => copyFile(path.join(main, file), path.join(copy, file)));
+      }
+
+
+    });
+
+  } catch(err){
+    console.error(err);
+  }
+}
+copyDir(main, copy);

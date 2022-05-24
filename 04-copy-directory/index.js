2a0f7cdb09229fbffb22d1ed4a3377af01c33250
Metamorg <egoronokasd@gmail.com>
2022-05-24 21:13:39 +0300

    fix;: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,22 +6,17 @@ const main = path.join(__dirname, 'files');
 const copy  = path.join(__dirname, 'files-copy');
 
 async function copyDir(main, copy) {
-  try{
-    await rm(copy, { recursive: true, force: true  });
-    await mkdir(copy);
+
+  await rm(copy, { recursive: true, force: true  });
+  await mkdir(copy);
   
-    await readdir(main, {withFileTypes: true},(err, items) =>{
-      if(err) {
-        console.log(err);
-      } else {
-        items.forEach(file => copyFile(path.join(main, file), path.join(copy, file)));
-      }
-
-
+  await readdir(main)
+    .then((items) => {
+      items.forEach(file => copyFile(path.join(main, file), path.join(copy, file)));
     });
+   
 
-  } catch(err){
-    console.error(err);
-  }
+
 }
+
 copyDir(main, copy);
